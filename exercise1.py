3824e6f06c2459e38ea24fc3cc69e268e9e35b15
TriKnight <nvn.knightdragon@gmail.com>
2026-01-30 13:57:44 +0700

    adding code ex1, ex2

--- exercise1.py
+++ exercise1.py
@@ -1,1 +1,25 @@
-import numpy as np
+# Object-oriented approach
+class TemperatureSensor:
+    def __init__(self):
+        self._value = 0
+        self._error = False
+    
+    def read(self):
+        try:
+            self._value = self._get_raw_value()
+            self._error = False
+        except Exception as e:
+            self._error = True
+            self._value = -999
+    
+    def is_valid(self):
+        return not self._error
+    
+    def get_value(self):
+        return self._value if self.is_valid() else None
+
+# Usage is cleaner and more intuitive
+sensor = TemperatureSensor()
+sensor.read()
+if sensor.is_valid():
+    print(sensor.get_value())
--- exercise2.py
+++ exercise2.py
@@ -1,1 +1,14 @@
-import numpy as np
+
+class TemperatureSensor:
+    def read(self):
+        return "24°C"
+
+class PressureSensor:
+    def read(self):
+        return "101.3 kPa"
+
+# Polymorphism in action
+sensors = [TemperatureSensor(), PressureSensor()]
+
+for s in sensors:
+    print(s.read()) # Same method name, different behaviors
